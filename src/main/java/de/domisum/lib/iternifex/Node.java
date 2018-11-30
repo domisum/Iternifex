a44306aef2439859a4d29d9046e35b1f27f3002b
domisum <dominik.weissenseel@gmail.com>
2018-11-30 16:11:05 +0100

    Added pathfinding node and edge

--- src/main/java/de/domisum/lib/iternifex/Node.java
+++
@@ -1,6 +0,0 @@
-package de.domisum.lib.iternifex;
-
-public class Node
-{
-	
-}
---
+++ src/main/java/de/domisum/lib/iternifex/PathfindingEdge.java
@@ -0,0 +1,32 @@
+package de.domisum.lib.iternifex;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Set;
+
+public interface PathfindingEdge<NodeT extends PathfindingNode<NodeT>>
+{
+
+	/**
+	 * Returns the nodes this edge connects. Always returns two nodes.
+	 *
+	 * @return the nodes this edge connects
+	 */
+	Set<NodeT> getNodes();
+
+	default NodeT getOther(NodeT node)
+	{
+		ArrayList<NodeT> nodes = new ArrayList<>(getNodes());
+
+		if(Objects.equals(nodes.get(0), node))
+			return nodes.get(1);
+		else if(Objects.equals(nodes.get(1), node))
+			return nodes.get(0);
+		else
+			throw new IllegalArgumentException("edge does not connect to node "+node);
+	}
+
+
+	double getWeight();
+
+}
