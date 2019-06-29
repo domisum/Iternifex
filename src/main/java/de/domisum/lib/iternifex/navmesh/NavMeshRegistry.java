20900b79cdfca432b52d84b57a3b8723b176133d
domisum <dominik.weissenseel@gmail.com>
2019-06-29 12:50:29 +0200

    Added NavMeshRegistry

---
+++ src/main/java/de/domisum/lib/iternifex/navmesh/NavMeshRegistry.java
@@ -0,0 +1,46 @@
+package de.domisum.lib.iternifex.navmesh;
+
+import de.domisum.lib.auxilium.data.container.math.Vector3D;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NavMeshRegistry
+{
+
+	private final Map<String, NavMesh> navMeshes = new HashMap<>();
+
+
+	// GETTERS
+	public NavMesh getNavMesh(String id)
+	{
+		return navMeshes.get(id);
+	}
+
+	public NavMesh getNavMeshAt(Vector3D location)
+	{
+		for(NavMesh navMesh : navMeshes.values())
+			if(navMesh.containsLocation(location))
+				return navMesh;
+
+		return null;
+	}
+
+
+	// REGISTRATION
+	public void register(NavMesh navMesh)
+	{
+		navMeshes.put(navMesh.getId(), navMesh);
+	}
+
+	public void unregister(NavMesh navMesh)
+	{
+		unregister(navMesh.getId());
+	}
+
+	public void unregister(String navMeshId)
+	{
+		navMeshes.get(navMeshId);
+	}
+
+}
