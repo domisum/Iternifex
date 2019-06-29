4f3bb1cca40a19ca0ab45eefa5307af1a856e0d1
domisum <dominik.weissenseel@gmail.com>
2019-06-29 20:08:20 +0200

    Added triangle traverser edge case

--- src/main/java/de/domisum/lib/iternifex/navmesh/pathfinding/NavMeshTriangleTraverser.java
+++ src/main/java/de/domisum/lib/iternifex/navmesh/pathfinding/NavMeshTriangleTraverser.java
@@ -3,6 +3,7 @@ package de.domisum.lib.iternifex.navmesh.pathfinding;
 import de.domisum.lib.auxilium.data.container.math.Vector3D;
 import de.domisum.lib.iternifex.navmesh.components.NavMeshTriangle;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NavMeshTriangleTraverser
@@ -10,6 +11,9 @@ public class NavMeshTriangleTraverser
 
 	public List<PathSegment> traverse(List<NavMeshTriangle> triangleSequence, Vector3D startLocation, Vector3D endLocation)
 	{
+		if(triangleSequence.size() == 1)
+			return Arrays.asList(new PathSegment(startLocation, endLocation, MovementType.WALK));
+
 		// TODO
 		return null;
 	}
